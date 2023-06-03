be571a91907fdca551f31a272551a45ce3e38335
sintecs <sintecs@list.ru>
2023-06-03 16:30:16 +0300

    change except exceptions

--- routes/quiz.py
+++ routes/quiz.py
@@ -1,5 +1,5 @@
 import asyncio
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from database.connection import get_session
 from models.quiz import Question, QuestionsCount
 from services.services import add_questions_to_db
@@ -16,9 +16,19 @@ async def create_question(count: QuestionsCount,
                           ) -> dict:
     """ Функция создания вопросов и отправки
     в ответ одного последнего добавленного в БД вопроса. """
-    last_question = (session.query(Question)
-                     .order_by(desc(Question.id)).first())
-    question = last_question.to_dict() if last_question else None
-    # Чтобы пользователь не ждать записи в БД всех вопросов, а получил ответ.
-    asyncio.create_task(add_questions_to_db(count.questions_num, session))
+    try:
+        last_question = (session.query(Question)
+                         .order_by(desc(Question.id)).first())
+        question = last_question.to_dict() if last_question else None
+        # Запускаем асинхронно, чтобы пользователь сразу получил ответ.
+        asyncio.create_task(add_questions_to_db(count.questions_num, session))
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Произошла ошибка сервера.")
+    if question is None:
+        raise HTTPException(
+            status_code=status.HTTP_204_NO_CONTENT,
+            detail="Мы не можем Вам прислать опрос, повторите запрос."
+        )
     return question
